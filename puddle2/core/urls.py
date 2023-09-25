0c6cfb5ad8accaa405b391fcebe99ae455804207
ogochukwu <ugwuogochukwu01@gmail.com>
2023-09-25 10:30:39 +0100

    first django

---
+++ puddle2/core/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path 
+
+from .views import signup, index, add_to_cart
+from django.contrib.auth import views as auth_views 
+from .forms import LoginForm
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', index, name = 'index'),
+    path('signup/', signup, name = 'signup'), 
+    path('add_to_cart/<int:item_id>/', add_to_cart, name='add_to_cart'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'core/login.html', authentication_form = LoginForm), name = 'login')
+
+]
